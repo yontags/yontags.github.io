0a644e94d2100b2194a43c1a16f9d61f30fd234b
tony <yontags@gmail.com>
2022-11-07 04:41:00 +0900

    graduation

---
+++ js/weather.js
@@ -0,0 +1,20 @@
+const API_KEY = "fe39d7413b5857e9e13ff3c3a73a4896";
+
+function onGeoSuccess(position) {
+  const lat = position.coords.latitude;
+  const lng = position.coords.longitude;
+  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=${API_KEY}&units=metric`;
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+      const weather = document.querySelector("#weather span:first-child");
+      const city = document.querySelector("#weather span:last-child");
+      city.innerText = data.name;
+      weather.innerText = `${data.weather[0].main} / ${data.main.temp}`;
+    });
+}
+function onGeoError() {
+  alert("Can't find you. No weather for you.");
+}
+
+navigator.geolocation.getCurrentPosition(onGeoSuccess, onGeoError);
